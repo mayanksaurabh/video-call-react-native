e4f7970b56718629ca4f96f3eb6fff6555d17c6d
mayank_saurabh <mayank.saurabh@elucidata.io>
2020-04-29 21:30:51 +0530

    [WIP]:Dummy video call app

--- MyTestApp/videoCallComponent/component/authScreen/index.js
+++ MyTestApp/videoCallComponent/component/authScreen/index.js
@@ -18,12 +18,12 @@ export default class AuthScreen extends PureComponent {
 
     login = currentUser => {
                 const _onSuccessLogin = () => {
-                console.log("this.props auth Screen ------->",this.props)
+                console.log("this.props auth Screen ------->",this.props);
                 const {navigation} = this.props;
                 const opponentsIds = users
                     .filter(opponent => opponent.id !== currentUser.id)
                     .map(opponent => opponent.id);
-            
+                console.log("opponent user ids auth screen------>", opponentsIds);    
                 navigation.push('VideoScreen', {opponentsIds});
             };
 
@@ -37,5 +37,37 @@ export default class AuthScreen extends PureComponent {
             .then(_onSuccessLogin)
             .catch(_onFailLogin)
             .then(() => this.setIsLogging(false));
-    };        
+    };
+    
+    render() {
+        const {isLogging} = this.state;
+        return (
+            <View style={[styles.container, styles.f1]}>
+                <StatusBar backgroundColor="white" barStyle="dark-content" />
+                <SafeAreaView style={[styles.centeredChildren, styles.f1]}>
+                <View
+                    style={[
+                    styles.f1,
+                    styles.centeredChildren,
+                    {flexDirection: 'row'},
+                    ]}>
+                    <Text>{isLogging ? 'Connecting... ' : 'Video Chat'}</Text>
+                    {isLogging && <ActivityIndicator size="small" color="#1198d4" />}
+                </View>
+                </SafeAreaView>
+                <SafeAreaView style={[styles.authBtns, styles.f1]}>
+                {users.map(user => (
+                    <TouchableOpacity key={user.id} onPress={() => this.login(user)}>
+                    <View
+                        style={[styles.authBtn(user.color), styles.centeredChildren]}>
+                        <Text style={styles.authBtnText}>
+                        {`Log in as ${user.name}`}
+                        </Text>
+                    </View>
+                    </TouchableOpacity>
+                ))}
+                </SafeAreaView>
+            </View>
+        );
+    }
 }
